52b797a6b66136e33c9cfcb306be4ba9e99381b2
Shaun Kelly <shaunkelly@gmail.com>
2015-09-06 11:19:31 -0500

    Get appview working with data.json

--- dev/assets/js/views/appview.js
+++ dev/assets/js/views/appview.js
@@ -6,11 +6,20 @@ app.AppView = Backbone.View.extend({
   initialize: function() {
     console.log("Hello world");
     this.listenTo(app.InspirationList, 'add', this.addInspirationView);
-    app.InspirationList.add({title: "Don't make shitty shit that's shitty."});
-    app.InspirationList.add({title: "There's no “I” in quit."});
-    app.InspirationList.add({title: "Abandon all Hope ye who enter here."});
+    // app.InspirationList.add({title: "Don't make shitty shit that's shitty."});
+    // app.InspirationList.add({title: "There's no “I” in quit."});
+    // app.InspirationList.add({title: "Abandon all Hope ye who enter here."});
 
-
+    $.getJSON('assets/data/data.json')
+      .done(function(data) {
+        console.log('success');
+        console.log(data);
+        app.InspirationList.set(data);
+      })
+      .fail(function(data) {
+        console.log('fail');
+        console.log(data);
+      });
   },
   render: function() {
     console.log('appview render');
